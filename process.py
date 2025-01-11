041ae01799ffaad9f46dfc548cd99573479bb2b7
Silous <silous666@gmail.com>
2025-01-11 19:05:28 +0100

    add function to get excluded words of a game

--- process.py
+++ process.py
@@ -1,7 +1,11 @@
 """"main process of the application"""
 
+# -------------------- Import Lib Standard -------------------
 import json
 from typing import Any
+
+# -------------------- Import Lib User -------------------
+from api import google_sheet_api as gsa
 
 data: dict[str, Any] = {}
 
@@ -13,3 +17,19 @@ def load_json() -> dict[str, Any]:
     """
     with open("./json_data_jeux.json", "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def get_list_specific_word(sheet_index: int) -> list[str] | int:
+    """get the list of specific words of a game in the sheet
+
+    Args:
+        sheet_index (int): index related to the game wanted,
+
+    Returns:
+        list[str]: list of specific words
+    """
+    gsa.open_spreadsheet(ID_SHEET_DICT_GAME)
+    result: list[list[str]] | int = gsa.get_sheet(sheet_index)
+    if isinstance(result, int):
+        return result
+    return [item for sublist in result for item in sublist]
