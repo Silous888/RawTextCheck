ce59b7d19a87ff7721079ae421e90e65a67ff5d9
Silous <silous666@gmail.com>
2025-01-11 18:34:42 +0100

    add id of sheet for exclude words

--- process.py
+++ process.py
@@ -5,6 +5,8 @@ from typing import Any
 
 data: dict[str, Any] = {}
 
+ID_SHEET_DICT_GAME: str = "1tjUT3K4kX5_ArT6GXXovWEMf1JmeQRr6_JiqxBCSVrc"
+
 
 def load_json() -> dict[str, Any]:
     """load json data
