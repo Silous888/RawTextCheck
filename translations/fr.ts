9e61672b6fa9ba5bebc64e49f7fbcaece21e00b1
Silous <silous666@gmail.com>
2025-05-29 23:05:32 +0200

    update translation files generation

--- translations/fr.ts
+++ translations/fr.ts
@@ -6,233 +6,173 @@
     <message>
         <location filename="../qt/confirm_exit/confirm_exit.ui" line="26"/>
         <source>DreamTeamTrad - FautesCheck - Sauvegarder termes ?</source>
-        <translation>DreamTeamTrad - FautesCheck - Sauvegarder termes ?</translation>
+        <translation type="obsolete">DreamTeamTrad - FautesCheck - Sauvegarder termes ?</translation>
     </message>
     <message>
         <location filename="../qt/confirm_exit/confirm_exit.ui" line="47"/>
         <source>Que faire ?</source>
-        <translation>Que faire ?</translation>
+        <translation type="obsolete">Que faire ?</translation>
     </message>
     <message>
         <location filename="../qt/confirm_exit/confirm_exit.ui" line="68"/>
         <source>Des termes n'ont pas été sauvegardés sur le google sheet.</source>
-        <translation>Des termes n'ont pas été sauvegardés sur le google sheet.</translation>
+        <translation type="obsolete">Des termes n'ont pas été sauvegardés sur le google sheet.</translation>
     </message>
     <message>
         <location filename="../qt/confirm_exit/confirm_exit.ui" line="89"/>
         <source>Quitter sans sauvegarder</source>
-        <translation>Quitter sans sauvegarder</translation>
+        <translation type="obsolete">Quitter sans sauvegarder</translation>
     </message>
     <message>
         <location filename="../qt/dialog_excluded_words/dialog_excluded_words.ui" line="117"/>
         <source>Sauvegarder et quitter</source>
-        <translation>Sauvegarder et quitter</translation>
+        <translation type="obsolete">Sauvegarder et quitter</translation>
     </message>
     <message>
         <location filename="../qt/dialog_excluded_words/dialog_excluded_words.ui" line="14"/>
         <source>DreamTeamTrad - FautesCheck - Liste mots exclus</source>
-        <translation>DreamTeamTrad - FautesCheck - Liste mots exclus</translation>
+        <translation type="obsolete">DreamTeamTrad - FautesCheck - Liste mots exclus</translation>
     </message>
     <message>
         <location filename="../qt/dialog_excluded_words/dialog_excluded_words.ui" line="60"/>
         <source>mots</source>
-        <translation>mots</translation>
+        <translation type="obsolete">mots</translation>
     </message>
     <message>
         <location filename="../qt/dialog_excluded_words/dialog_excluded_words.ui" line="95"/>
         <source>Réinitialiser changements</source>
-        <translation>Réinitialiser changements</translation>
+        <translation type="obsolete">Réinitialiser changements</translation>
     </message>
 </context>
 <context>
     <name>DialogExcludedWords</name>
-    <message>
-        <location filename="../qt/dialog_excluded_words/dialog_excluded_words.py" line="97"/>
-        <source>Supprimer</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="14"/>
         <source>DreamTeamTrad - FautesCheck</source>
-        <translation>DreamTeamTrad - FautesCheck</translation>
+        <translation type="obsolete">DreamTeamTrad - FautesCheck</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="33"/>
         <source>Choix du jeu et du google sheet</source>
-        <translation>Choix du jeu et du google sheet</translation>
+        <translation type="obsolete">Choix du jeu et du google sheet</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="67"/>
         <source>Dictionnaire termes du jeu</source>
-        <translation>Dictionnaire termes du jeu</translation>
+        <translation type="obsolete">Dictionnaire termes du jeu</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="111"/>
         <source>mettre ici URL google sheet à traiter</source>
-        <translation type="unfinished">mettre ici URL google sheet à traiter</translation>
+        <translation type="obsolete">mettre ici URL google sheet à traiter</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="133"/>
         <source>Colonne du français :</source>
-        <translation>Colonne du français :</translation>
+        <translation type="obsolete">Colonne du français :</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="185"/>
         <source>name_sheet_opened</source>
-        <translation>name_sheet_opened</translation>
+        <translation type="obsolete">name_sheet_opened</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="246"/>
         <source>Mise à jour des fautes trouvées</source>
-        <translation>Mise à jour des fautes trouvées</translation>
+        <translation type="obsolete">Mise à jour des fautes trouvées</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="258"/>
         <source>Vérificateur de OpenOffice. Peut analyser les phrases. Un peu lent.</source>
-        <translation>Vérificateur de OpenOffice. Peut analyser les phrases. Un peu lent.</translation>
+        <translation type="obsolete">Vérificateur de OpenOffice. Peut analyser les phrases. Un peu lent.</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="261"/>
         <source>Mise à jour &quot;LanguageTool&quot;</source>
-        <translation>Mise à jour &quot;LanguageTool&quot;</translation>
+        <translation type="obsolete">Mise à jour &quot;LanguageTool&quot;</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="397"/>
         <source>last_update</source>
-        <translation>last_update</translation>
+        <translation type="obsolete">last_update</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="531"/>
         <source>Recherche</source>
-        <translation>Recherche</translation>
+        <translation type="obsolete">Recherche</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="299"/>
         <source>Rechercher</source>
-        <translation>Rechercher</translation>
+        <translation type="obsolete">Rechercher</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="332"/>
         <source>Remplacer</source>
-        <translation>Remplacer</translation>
+        <translation type="obsolete">Remplacer</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="350"/>
         <source>Dans les résultats de la recherche, remplacer </source>
-        <translation>Dans les résultats de la recherche, remplacer</translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.ui" line="378"/>
-        <source>par</source>
-        <translation></translation>
+        <translation type="obsolete">Dans les résultats de la recherche, remplacer</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="429"/>
         <source>Pour limiter les appels à l'API de Google Sheet, uploader plusieurs termes d'un coup.</source>
-        <translation>Pour limiter les appels à l'API de Google Sheet, uploader plusieurs termes d'un coup.</translation>
+        <translation type="obsolete">Pour limiter les appels à l'API de Google Sheet, uploader plusieurs termes d'un coup.</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.py" line="611"/>
         <source>Pas de termes à uploader</source>
-        <translation>Pas de termes à uploader</translation>
+        <translation type="obsolete">Pas de termes à uploader</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="473"/>
         <source>Vérification</source>
-        <translation>Vérification</translation>
+        <translation type="obsolete">Vérification</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="565"/>
         <source>1</source>
-        <translation>1</translation>
+        <translation type="obsolete">1</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.py" line="223"/>
         <source>fichier</source>
-        <translation>fichier</translation>
+        <translation type="obsolete">fichier</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.py" line="223"/>
         <source>ligne</source>
-        <translation>ligne</translation>
+        <translation type="obsolete">ligne</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.py" line="223"/>
         <source>faute</source>
-        <translation>faute</translation>
+        <translation type="obsolete">faute</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="606"/>
         <source>menu</source>
-        <translation>menu</translation>
+        <translation type="obsolete">menu</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="615"/>
         <source>éditer le json des jeux</source>
-        <translation>éditer le json des jeux</translation>
+        <translation type="obsolete">éditer le json des jeux</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.ui" line="620"/>
         <source>configuration</source>
-        <translation>configuration</translation>
+        <translation type="obsolete">configuration</translation>
     </message>
     <message>
         <location filename="../qt/mainwindow/mainwindow.py" line="60"/>
         <source>-- Choisissez un jeu --</source>
-        <translation>-- Choisissez un jeu --</translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="113"/>
-        <source> terme(s) à upload</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="119"/>
-        <source>Veuillez uploader les termes avant de changer de jeu</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="484"/>
-        <source>Ajouter aux termes du jeu</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="457"/>
-        <source>Ajouter au dictionnaire de la méthode</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="487"/>
-        <source>Faute traitée</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="459"/>
-        <source>Ajouter en tant que lettre autorisée</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="460"/>
-        <source>Ajouter en tant que ponctuation autorisée</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="486"/>
-        <source>ignorer </source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="486"/>
-        <source> pour ce jeu</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qt/mainwindow/mainwindow.py" line="510"/>
-        <source>Exclure des résultats de la recherche</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">-- Choisissez un jeu --</translation>
     </message>
 </context>
 </TS>
--- translations/translations.pro
+++ translations/translations.pro
@@ -1,12 +1,14 @@
 SOURCES = \
     ../qt/confirm_exit/confirm_exit.py \
     ../qt/dialog_excluded_words/dialog_excluded_words.py \
-    ../qt/mainwindow/mainwindow.py
+    ../qt/mainwindow/mainwindow.py \
+    ../qt/project_manager/project_manager.py
 
 FORMS = \
     ../qt/confirm_exit/confirm_exit.ui \
     ../qt/dialog_excluded_words/dialog_excluded_words.ui \
-    ../qt/mainwindow/mainwindow.ui
+    ../qt/mainwindow/mainwindow.ui \
+    ../qt/project_manager/project_manager.ui
 
 TRANSLATIONS = fr.ts en.ts
 
