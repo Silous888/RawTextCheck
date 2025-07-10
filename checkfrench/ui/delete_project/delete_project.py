71ef780c80ff3b17d3ff2ef751c3319cf1ce454e
Silous <silous666@gmail.com>
2025-07-10 23:24:31 +0200

    doc delete_project

--- checkfrench/ui/delete_project/delete_project.py
+++ checkfrench/ui/delete_project/delete_project.py
@@ -1,3 +1,16 @@
+
+"""
+File        : delete_project.py
+Author      : Silous
+Created on  : 2025-06-09
+Description : Dialog for deleting a project in the application.
+
+This module provides a dialog that prompts the user to confirm the deletion of a project.
+"""
+
+
+# == Imports ==================================================================
+
 # -------------------- Import Lib Tier -------------------
 from PyQt5.QtWidgets import QDialog
 
@@ -6,9 +19,20 @@ from checkfrench.script import json_projects
 from checkfrench.ui.delete_project.Ui_delete_project import Ui_Dialog_deleteProject
 
 
+# == Classes ==================================================================
+
 class DialogDeleteProject(QDialog):
+    """Dialog for deleting a project.
+    This dialog prompts the user to confirm the deletion of a project and handles the deletion logic.
+    Attributes:
+        m_project_name (str): The name of the project to be deleted.
+    """
 
     def __init__(self, project_name: str) -> None:
+        """Initialize the DialogDeleteProject with the project name.
+        Args:
+            project_name (str): The name of the project to be deleted.
+        """
         super(QDialog, self).__init__()
         self.ui = Ui_Dialog_deleteProject()
 
@@ -27,15 +51,16 @@ class DialogDeleteProject(QDialog):
     # -------------------- Slots -------------------
 
     def pushButton_yes_clicked(self) -> None:
-        """Handle the creation of a new project."""
+        """Slot for the Yes button click."""
         self.delete_project()
         self.close()
 
     def pushButton_no_clicked(self) -> None:
-        """Handle the creation of a new project."""
+        """Slot for the No button click."""
         self.close()
 
     # -------------------- Methods -------------------
 
     def delete_project(self) -> None:
+        """Delete the project with the specified name."""
         json_projects.delete_entry(self.m_project_name)
