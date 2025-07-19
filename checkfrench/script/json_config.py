f4dde37af41460bc78f31ab149733f2dab2b22ae
Silous <silous666@gmail.com>
2025-07-19 18:23:27 +0200

    setter and getter for app config #25

--- checkfrench/script/json_config.py
+++ checkfrench/script/json_config.py
@@ -26,12 +26,51 @@ logger: Logger = get_logger(__name__)
 # == Functions ================================================================
 
 def create_json() -> None:
+    """create json for app config if the file
+    doesn't exist
+    """
     if os.path.exists(JSON_CONFIG_PATH):
         return
 
     os.makedirs(CONFIG_FOLDER, exist_ok=True)
 
-    data: ItemConfig = ItemConfig(language=LANGUAGES[0][0], theme=THEMES[0][0], hidden_column=[], last_project="")
+    data: ItemConfig = ItemConfig(language=LANGUAGES[0][0],
+                                  theme=THEMES[0][0],
+                                  hidden_column=[],
+                                  last_project="")
+    save_data(data)
+    logger.info("Created %s.", JSON_CONFIG_PATH)
+
+
+def save_data(data: ItemConfig) -> None:
     with open(JSON_CONFIG_PATH, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    logger.info("Created %s.", JSON_CONFIG_PATH)
+
+
+def load_data() -> ItemConfig:
+    with open(JSON_CONFIG_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)[0]
+
+
+def set_language(language: str) -> None:
+    data: ItemConfig = load_data()
+    data["language"] = language
+    save_data(data)
+
+
+def set_theme(theme: str) -> None:
+    data: ItemConfig = load_data()
+    data["theme"] = theme
+    save_data(data)
+
+
+def set_hidden_column(hidden_column: list[str]) -> None:
+    data: ItemConfig = load_data()
+    data["hidden_column"] = hidden_column
+    save_data(data)
+
+
+def set_last_project(last_project: str) -> None:
+    data: ItemConfig = load_data()
+    data["last_project"] = last_project
+    save_data(data)
