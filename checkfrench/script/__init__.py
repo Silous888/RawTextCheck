512284a84ba721ec26185d5422c0e2493e2b9200
Silous <silous666@gmail.com>
2025-07-22 16:19:00 +0200

    fix created date for script init.py

--- checkfrench/script/__init__.py
+++ checkfrench/script/__init__.py
@@ -1,7 +1,7 @@
 """
 Package     : script
 Author      : Silous
-Created on  : 2025-04-22
+Created on  : 2025-07-10
 Description : Various scripts and utilities for the application.
 
 Probably needs some sorting
